d8a40ff05f3809cc413a90071822e1f3745c23b3
ibaslogic <ibaskunle@gmail.com>
2020-03-19 12:22:00 +0100

    Updates

---
+++ precache-manifest.753a2bb96bc86c1493b926d274a09088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa10d1768c68cfab207837614e3e3012",
+    "url": "/react-hooks-starter-project/index.html"
+  },
+  {
+    "revision": "de3d72fa26af3061344e",
+    "url": "/react-hooks-starter-project/static/css/main.b1e8f748.chunk.css"
+  },
+  {
+    "revision": "c70d010e39ea70bc7d8e",
+    "url": "/react-hooks-starter-project/static/js/2.9ce9ae2b.chunk.js"
+  },
+  {
+    "revision": "de3d72fa26af3061344e",
+    "url": "/react-hooks-starter-project/static/js/main.4689a779.chunk.js"
+  },
+  {
+    "revision": "bf9ebb3eccd5cc0f6815",
+    "url": "/react-hooks-starter-project/static/js/runtime-main.2e4aab7a.js"
+  }
+]);
